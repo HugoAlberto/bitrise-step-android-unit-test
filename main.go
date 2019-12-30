f1bef9efa22fd77d3b176ecbc5e509d46ac7df72
HBoulouk <hugo.boulouk@instant-system.com>
2019-12-30 10:54:49 +0100

    revert custom fun

--- main.go
+++ main.go
@@ -9,9 +9,8 @@ import (
 	"time"
 
 	"github.com/bitrise-io/go-android/cache"
-	"github.com/bitrise-io/go-android/gradle"
+	"github.com/HugoAlberto/go-android/gradle"
 	"github.com/bitrise-io/go-steputils/stepconf"
-	"github.com/bitrise-io/go-utils/command"
 	"github.com/bitrise-io/go-utils/log"
 	"github.com/bitrise-io/go-utils/pathutil"
 	"github.com/bitrise-io/go-utils/sliceutil"
@@ -150,72 +149,6 @@ func tryExportTestAddonArtifact(artifactPth, outputDir string, lastOtherDirIdx i
 	return lastOtherDirIdx
 }
 
-// GetVariants ...
-func (task *Task) GetAllVariants() (Variants, error) {
-	tasksOutput, err := getGradleOutput(task.project.location, "tasks", "--all", "--console=plain", "--quiet")
-	if err != nil {
-		return nil, fmt.Errorf("%s, %s", tasksOutput, err)
-	}
-	return task.parseAllVariants(tasksOutput), nil
-}
-
-func (task *Task) parseAllVariants(gradleOutput string) Variants {
-	//example gradleOutput:
-	//"
-	// lintMyflavorokStaging - Runs lint on the MyflavorokStaging build.
-	// lintMyflavorRelease - Runs lint on the MyflavorRelease build.
-	// lintVitalMyflavorRelease - Runs lint on the MyflavorRelease build.
-	// lintMyflavorStaging - Runs lint on the MyflavorStaging build."
-	tasks := Variants{}
-lines:
-	for _, l := range strings.Split(gradleOutput, "\n") {
-		// l: " lintMyflavorokStaging - Runs lint on the MyflavorokStaging build."
-		l = strings.TrimSpace(l)
-		// l: "lintMyflavorokStaging - Runs lint on the MyflavorokStaging build."
-		if l == "" {
-			continue
-		}
-		// l: "lintMyflavorokStaging"
-		l = strings.Split(l, " ")[0]
-		var module string
-
-		log.Warnf("Gradle Tasks: %s", l)
-		split := strings.Split(l, ":")
-		log.Warnf("Variant module split: %s", split)
-		size := len(split)
-
-		log.Warnf("Variant module size: %s", size)
-		if size > 1 {
-			module = strings.Join(split[:size - 1], ":")
-			l = split[size - 1]
-		}
-		// module removed if any
-		if strings.HasPrefix(l, task.name) {
-			// task.name: "lint"
-			// strings.HasPrefix will match lint and lintVital prefix also, we won't need lintVital so it is a conflict
-			for _, conflict := range conflicts[task.name] {
-				if strings.HasPrefix(l, conflict) {
-					// if line has conflicting prefix don't do further checks with this line, skip...
-					continue lines
-				}
-			}
-			l = strings.TrimPrefix(l, task.name)
-			// l: "MyflavorokStaging"
-			if l == "" {
-				continue
-			}
-
-			tasks[module] = append(tasks[module], l)
-		}
-	}
-
-	for module, variants := range tasks {
-		tasks[module] = cleanStringSlice(variants)
-	}
-
-	return tasks
-}
-
 func main() {
 	var config Configs
 
@@ -238,7 +171,7 @@ func main() {
 	log.Infof("Variants:")
 	fmt.Println()
 
-	variants, err := testTask.GetAllVariants()
+	variants, err := testTask.GetVariants()
 	if err != nil {
 		failf("Failed to fetch variants, error: %s", err)
 	}
